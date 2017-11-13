ae423958cc02012e95d3b9f482c6b31d59999860
akellavolk <feanor28@yandex.ru>
2017-11-13 17:56:38 +0300

    Add ex 1.15 Scheme

--- Scheme/ex_1.15.scm
+++ Scheme/ex_1.15.scm
@@ -1,2 +1,14 @@
-(define (sin x)
-	)
+(define (cube x)
+  (* x x x))
+
+(define (p x) (- (* 3 x) (* 4 (cube x))))
+
+(define (sine angle count)
+  (cond ((not (> (abs angle) 0.1))
+              (display count)
+              angle)
+        (else (p (sine (/ angle 3.0) (+ 1 count))))))
+      
+(sine 12.15 0)
+
+; При вычислении синуса 12.15 радиан, процедура p вызывается 5 раз
